4f23b0c3741e714795c2bfd64df0ed9a823b154e
Bogdan Vladutu <bogdanvladutu@gmail.com>
2023-04-12 18:05:17 +0300

    Coroutines

--- Coroutines/src/main/kotlin/Main.kt
+++ Coroutines/src/main/kotlin/Main.kt
@@ -18,4 +18,19 @@ fun main() {
 //    println("Right back at ya!")
 //  }
 //  Thread.sleep(1000)
+
+  // Coroutine will start when is needed, on .join(); Will show: Ping, Pong, Ping
+//  val job1 = GlobalScope.launch(start = CoroutineStart.LAZY) {
+//    delay(200)
+//    println("Pong")
+//    delay(200)
+//  }
+//  GlobalScope.launch {
+//    delay(200)
+//    println("Ping")
+//    job1.join()
+//    println("Ping")
+//    delay(200)
+//  }
+//  Thread.sleep(1000)
 }
