1547b4c9f2da5f32bff0258740745b8e3201141d
Bogdan Vladutu <bogdanvladutu@gmail.com>
2023-04-12 18:01:32 +0300

    Kotlin - Introduce coroutines

---
+++ Coroutines/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import kotlinx.coroutines.*
+
+fun main() {
+
+  // Starting 10.00 corroutines on a different threads
+//  (1..10000).forEach {
+//    GlobalScope.launch {
+//      val threadName = Thread.currentThread().name
+//      println("$it printed on thread ${threadName}")
+//    }
+//  }
+//  Thread.sleep(1000)
+
+  // Delay in a coroutine
+//  GlobalScope.launch {
+//    println("Hello coroutine!")
+//    delay(500)
+//    println("Right back at ya!")
+//  }
+//  Thread.sleep(1000)
+}
