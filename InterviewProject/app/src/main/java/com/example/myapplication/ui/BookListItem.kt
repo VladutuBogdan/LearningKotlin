1339c76d3c6939aa25a118860af9f0b7a5287f16
Bogdan Vladutu <bogdanvladutu@gmail.com>
2023-06-03 10:34:55 +0300

    Compose List Design

--- InterviewProject/app/src/main/java/com/example/myapplication/ui/BookListItem.kt
+++ InterviewProject/app/src/main/java/com/example/myapplication/ui/BookListItem.kt
@@ -2,10 +2,12 @@ package com.example.myapplication
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -14,25 +16,34 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.myapplication.data.Book
 
 @Composable
 fun BookListItem(book: Book) {
-    Button(onClick = {
+    Button(
+        shape = RoundedCornerShape(5),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.LightGray
+        ),
+        modifier = Modifier.padding(5.dp),
+        onClick = {
         // TODO: SELECT SELECTED BOOK IN STATEFLOW
     }) {
         Card(
-            shape = RoundedCornerShape(5),
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 10.dp
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Transparent
             )
         ) {
             Row(
                 horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(4.dp)
             ) {
                 BookImage(book)
@@ -41,6 +52,7 @@ fun BookListItem(book: Book) {
                     style = TextStyle(
                         color = Color.Black,
                         fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
                     ),
                     modifier = Modifier.padding(horizontal = 3.dp)
                 )
@@ -53,8 +65,9 @@ fun BookListItem(book: Book) {
 fun BookImage(book: Book) {
     AsyncImage(
         model = book.image.url,
+        placeholder = painterResource(R.drawable.loading),
         contentDescription = book.title,
-        modifier = Modifier.size(width = 2.dp, height = 2.dp),
+        modifier = Modifier.size(width = 100.dp, height = 80.dp),
         alignment = Alignment.Center,
         contentScale = ContentScale.Fit
     )
--- InterviewProject/app/src/main/java/com/example/myapplication/ui/BooksApplication.kt
+++ InterviewProject/app/src/main/java/com/example/myapplication/ui/BooksApplication.kt
@@ -1,12 +1,8 @@
 package com.example.myapplication
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.myapplication.data.Book
 
 @Composable
@@ -16,7 +12,6 @@ fun BooksApplication(books: List<Book>) {
             items = books,
             itemContent = {
                 BookListItem(book = it)
-                Spacer(modifier = Modifier.width(10.dp))
             }
         )
     }
