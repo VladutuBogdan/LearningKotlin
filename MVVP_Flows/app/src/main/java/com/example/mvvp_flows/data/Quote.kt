879ec1f7c6c654e9c59d583248fad7ade0fd3b1e
Bogdan Vladutu <bogdanvladutu@gmail.com>
2023-07-30 17:20:34 +0300

    Remove Testing Projects

--- MVVP_Flows/app/src/main/java/com/example/mvvp_flows/data/Quote.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mvvp_flows.data
-
-data class Quote(val quoteText: String, val author: String) {
-    override fun toString(): String {
-        return "$quoteText - $author";
-    }
-}
--- MVVP_LiveData/app/src/main/java/com/example/mvvp_livedata/data/Book.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mvvp_livedata.data
-
-data class Book(val book: String, val author: String) {
-    override fun toString(): String {
-        return "$book - $author"
-    }
-}
